3c7654212bc9c69704ffd3b6147ea61fe3b0c20d
Ashish Malla <ashishmalla45@gmail.com>
2022-11-03 21:54:33 -0600

    group tets using describe

--- src/components/tddExample/Tdd.test.tsx
+++ src/components/tddExample/Tdd.test.tsx
@@ -1,18 +1,19 @@
-// Tdd should render the text Hello 
+// Tdd should render the text Hello
 // and if a text is passed into the component it should render the hello followed by the name
 
-import { render, screen } from "@testing-library/react"
-import Tdd from "./Tdd"
+import { render, screen } from '@testing-library/react'
+import Tdd from './Tdd'
 
+describe('<Tdd />', () => {
+	test('renders correctly', () => {
+		render(<Tdd />)
+		const textEle = screen.getByText('Hello,')
+		expect(textEle).toBeInTheDocument()
+	})
 
-test("Tdd renders correctly", () => {
-  render(<Tdd />)
-  const textEle = screen.getByText('Hello,')
-  expect(textEle).toBeInTheDocument()
-})
-
-test('Tdd renders with a name', () => {
-  render(<Tdd name='Asiisii' />)
-  const textEle = screen.getByText('Hello, Asiisii')
-  expect(textEle).toBeInTheDocument()
+	test('renders with a name', () => {
+		render(<Tdd name='Asiisii' />)
+		const textEle = screen.getByText('Hello, Asiisii')
+		expect(textEle).toBeInTheDocument()
+	})
 })
