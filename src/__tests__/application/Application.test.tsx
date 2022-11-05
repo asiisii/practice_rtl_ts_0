f3b67950cfd426f75352f7981be023f25ac65ee0
Ashish Malla <ashishmalla45@gmail.com>
2022-11-04 22:16:29 -0600

    passing options to getByRole method

--- src/__tests__/application/Application.test.tsx
+++ src/__tests__/application/Application.test.tsx
@@ -4,16 +4,34 @@ import Application from '../../components/application/Application'
 describe('<Application />', () => {
 	it('renders correctly', () => {
 		render(<Application />)
-		const nameEle = screen.getByRole('textbox') //input default role is textbox
+
+		const pageHeading = screen.getByRole('heading', {
+			level: 1,
+		})
+		expect(pageHeading).toBeInTheDocument()
+
+		const sectionHeading = screen.getByRole('heading', {
+			level: 2,
+		})
+		expect(sectionHeading).toBeInTheDocument()
+
+		const nameEle = screen.getByRole('textbox', {
+			name: 'Name',
+		}) //input default role is textbox
 		expect(nameEle).toBeInTheDocument()
+
+		const bioEle = screen.getByRole('textbox', {
+			name: 'Bio',
+		}) // name options is case sensitive, use it when there are multiple elements with same role
+		expect(bioEle).toBeInTheDocument()
 
 		const jobLocationEle = screen.getByRole('combobox') //select default role is combobox
 		expect(jobLocationEle).toBeInTheDocument()
 
-    const termsEle = screen.getByRole('checkbox')
-    expect(termsEle).toBeInTheDocument()
+		const termsEle = screen.getByRole('checkbox')
+		expect(termsEle).toBeInTheDocument()
 
-    const submitBtnEle = screen.getByRole('button')
-    expect(submitBtnEle).toBeInTheDocument()
+		const submitBtnEle = screen.getByRole('button')
+		expect(submitBtnEle).toBeInTheDocument()
 	})
 })
--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -2,30 +2,38 @@ import React from 'react'
 
 const Application = () => {
 	return (
-		<form>
-			<div>
-				<label htmlFor='name'>Name</label>
-				<input type='text' id='name' />
-			</div>
-			<div>
-				<label htmlFor='job-location'>Job location</label>
-				<select id='job-location'>
-					<option value=''>Select a country</option>
-					<option value='US'>US</option>
-					<option value='Nepal'>Nepal</option>
-					<option value='China'>China</option>
-					<option value='Japan'>Japan</option>
-					<option value='Korea'>Korea</option>
-				</select>
-			</div>
-			<div>
-				<label>
-					<input type='checkbox' id='terms' />I agree to the terms and
-					conditions
-				</label>
-			</div>
-			<button>Submit</button>
-		</form>
+		<>
+			<h1>Job Application Form</h1>
+			<h2>Section 1</h2>
+			<form>
+				<div>
+					<label htmlFor='name'>Name</label>
+					<input type='text' id='name' />
+				</div>
+				<div>
+					<label htmlFor='bio'>Bio</label>
+					<textarea name='bio' id='bio' />
+				</div>
+				<div>
+					<label htmlFor='job-location'>Job location</label>
+					<select id='job-location'>
+						<option value=''>Select a country</option>
+						<option value='US'>US</option>
+						<option value='Nepal'>Nepal</option>
+						<option value='China'>China</option>
+						<option value='Japan'>Japan</option>
+						<option value='Korea'>Korea</option>
+					</select>
+				</div>
+				<div>
+					<label>
+						<input type='checkbox' id='terms' />I agree to the terms and
+						conditions
+					</label>
+				</div>
+				<button>Submit</button>
+			</form>
+		</>
 	)
 }
 
