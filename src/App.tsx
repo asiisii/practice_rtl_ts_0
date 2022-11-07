3a039398ed51cc1d0918190447a238b75ec84f55
Ashish Malla <ashishmalla45@gmail.com>
2022-11-06 21:53:06 -0700

    add Counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Application from "./components/application/Application"
+import Counter from "./components/counter/Counter"
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>RTL Practice</h1>
       <Application />
+      <Counter />
 		</div>
 	)
 }
