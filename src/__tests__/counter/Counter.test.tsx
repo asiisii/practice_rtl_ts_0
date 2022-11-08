e4fe1c55577daf33a1e06eddda9318f6e61f7733
Ashish Malla <ashishmalla45@gmail.com>
2022-11-07 22:09:17 -0700

    add test for user interactions

--- src/__tests__/counter/Counter.test.tsx
+++ src/__tests__/counter/Counter.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import user from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter/Counter'
 
 describe('<Counter />', () => {
@@ -25,7 +25,7 @@ describe('<Counter />', () => {
 	})
 
 	it('renders a count of 1 after clicking the increment button', async () => {
-		user.setup()
+		const user = userEvent.setup()
 		render(<Counter />)
 		const incrementBtn = screen.getByRole('button', {
 			name: /increment/i,
@@ -36,7 +36,7 @@ describe('<Counter />', () => {
 	})
 
 	it('renders a counter of 2 after clicking the increment button twice', async () => {
-		user.setup()
+		const user = userEvent.setup()
 		render(<Counter />)
 		const incrementBtn = screen.getByRole('button', {
 			name: /increment/i,
@@ -45,4 +45,40 @@ describe('<Counter />', () => {
 		const counterEle = screen.getByRole('heading')
 		expect(counterEle).toHaveTextContent('2')
 	})
+
+	it('renders a count of 10 after clicking the set button', async () => {
+		const user = userEvent.setup()
+		render(<Counter />)
+		const amtInputEle = screen.getByRole('spinbutton')
+		await user.type(amtInputEle, '10')
+		expect(amtInputEle).toHaveValue(10)
+
+		const setBtn = screen.getByRole('button', {
+			name: /set/i,
+		})
+		await user.click(setBtn)
+		const counterEle = screen.getByRole('heading')
+		expect(counterEle).toHaveTextContent('10')
+	})
+
+	it('elements are focused in the right order', async () => {
+		const user = userEvent.setup()
+		render(<Counter />)
+		const amtInputEle = screen.getByRole('spinbutton')
+		const setBtn = screen.getByRole('button', {
+			name: /set/i,
+		})
+		const incrementBtn = screen.getByRole('button', {
+			name: /increment/i,
+		})
+
+		await user.tab()
+		expect(incrementBtn).toHaveFocus()
+		await user.tab()
+		expect(amtInputEle).toHaveFocus()
+		await user.tab()
+		expect(setBtn).toHaveFocus()
+	})
+  
+
 })
