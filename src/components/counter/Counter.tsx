b8e4576544ee496a91771a775b5a07ee869ee6a7
Ashish Malla <ashishmalla45@gmail.com>
2022-11-07 22:09:03 -0700

    add input and button elements to test user interaction

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 const Counter = () => {
 	const [counter, setCounter] = useState(0)
+	const [amount, setAmount] = useState(0)
 	return (
 		<div>
 			<h1>{counter}</h1>
 			<button onClick={() => setCounter(counter + 1)}>Increment</button>
+			<input
+				type='number'
+				name='amount'
+				value={amount}
+				onChange={e => setAmount(parseInt(e.target.value))}
+			/>
+			<button onClick={() => setCounter(amount)}>Set</button>
 		</div>
 	)
 }
