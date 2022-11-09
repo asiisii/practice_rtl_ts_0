c67bf2ab4f5ca8951f867fbdf1b7eec236584069
Ashish Malla <ashishmalla45@gmail.com>
2022-11-08 19:54:13 -0700

    add AppProvider to control the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Application from "./components/application/Application"
 import Counter from "./components/counter/Counter"
+import { MuiMode } from "./components/mui/MuiMode"
+import { AppProviders } from "./components/providers/AppProviders"
 
 function App() {
 	return (
@@ -7,6 +9,9 @@ function App() {
 			<h1>RTL Practice</h1>
       <Application />
       <Counter />
+      <AppProviders>
+        <MuiMode />
+      </AppProviders>
 		</div>
 	)
 }
