8fbaf8211c9188459c97406e8859bca5d3b66443
Ashish Malla <ashishmalla45@gmail.com>
2022-11-09 21:48:41 -0700

    add test to fail the request and test for error message

--- src/__tests__/users/Users.test.tsx
+++ src/__tests__/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Users from '../../components/users/Users'
+import { server } from '../../mocks/server'
+import { rest } from 'msw'
 
 // use mock service worker
 describe('<Users />', () => {
@@ -9,9 +11,23 @@ describe('<Users />', () => {
 		expect(txtEle).toBeInTheDocument()
 	})
 
-  it('renders list of users', async () => {
+	it('renders list of users', async () => {
+		render(<Users />)
+		const users = await screen.findAllByRole('listitem')
+		expect(users).toHaveLength(3)
+	})
+
+	it('renders error', async () => {
+		server.use(
+			rest.get(
+				'https://jsonplaceholder.typicode.com/users',
+				(req, res, ctx) => {
+					return res(ctx.status(500))
+				}
+			)
+		)
     render(<Users />)
-    const users = await screen.findAllByRole('listitem')
-    expect(users).toHaveLength(3)
-  })
+    const err = await screen.findByText('Error fetching users')
+    expect(err).toBeInTheDocument()
+	})
 })
